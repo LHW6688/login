47815361b5c10ba1e8de95548edc51de56af59a0
李宏伟 <lihongwei@topgridcloud.com>
2020-01-10 11:28:36 +0800

    创建项目

---
+++ login/settings/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'''
+PROJECT_NAME：login 
+FILE:__init__.py 
+USERNAME: 李宏伟
+DATE:2020/1/10 
+TIME:上午11:18 
+PRODUCT_NAME:PyCharm 
+'''
+
+
---
+++ logs/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'''
+PROJECT_NAME：login 
+FILE:__init__.py 
+USERNAME: 李宏伟
+DATE:2020/1/10 
+TIME:上午11:17 
+PRODUCT_NAME:PyCharm 
+'''
+
+
